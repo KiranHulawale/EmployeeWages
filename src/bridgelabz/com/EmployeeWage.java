24d2aabc849c3e012988699dc17bf3b0ec4f3991
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-13 13:11:23 +0530

    Update EmployeeWage.java

--- src/bridgelabz/com/EmployeeWage.java
+++ src/bridgelabz/com/EmployeeWage.java
@@ -1,4 +1,12 @@
 package bridgelabz.com;
 
 public class EmployeeWage {
+   public static void main(String[] args) {
+
+        int empPresent=(int)(Math.floor(Math.random()*10%2));
+        if(empPresent==0)
+            System.out.println("Employee is absent");
+        else
+            System.out.println("Employee is present");
+    }
 }
